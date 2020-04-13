bf765830bdbede14fe246f88237b6ba81623dab2
George Travis <wewerelikesharks@gmail.com>
2020-04-13 09:20:30 -0700

    contact header in place and responsive

--- src/components/ContainerHeader/index.js
+++ src/components/ContainerHeader/index.js
@@ -5,6 +5,7 @@ function ContainerHeader(props) {
   return (
     <div className="container-header">
       <h2 className="container-header-title">{props.title}</h2>
+      <div>{props.contactDetails}</div>
     </div>
   );
 }
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,51 @@
 import React from 'react';
+import ContainerHeader from '../../components/ContainerHeader';
+import './style.css';
 
 function Contact() {
-  return <p>Contact</p>;
+  return (
+    <div>
+      <ContainerHeader
+        title="Contact me"
+        contactDetails={
+          <div className="large-screen">
+            <ul id="contact-links-contact">
+              <li>
+                <a
+                  href="https://github.com/agtravis/alexander-george-travis-resume/blob/master/alexander-george-travis-resume.pdf"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Résumé
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/agtravis"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  GitHub
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/alexander-george-travis/"
+                  className="link"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  LinkedIn
+                </a>
+              </li>
+            </ul>
+          </div>
+        }
+      />
+    </div>
+  );
 }
 
 export default Contact;
