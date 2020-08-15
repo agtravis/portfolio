e03c38b45b98f4dd02c8aed8e7697665183131e7
George Travis <wewerelikesharks@gmail.com>
2020-08-15 08:57:27 -0700

    added spaces to skills

--- src/context/SkillsContext.js
+++ src/context/SkillsContext.js
@@ -3,10 +3,10 @@ import React from 'react';
 const SkillsContext = React.createContext({
   HTML: (
     <span>
-      My comfort zones: <span className="emphasize">React & JSX</span>, Tacos,
+      My comfort zones: <span className="emphasize">React & JSX</span>, Tacos,{' '}
       <span className="emphasize">JavaScript</span>, Cats,{' '}
       <span className="emphasize">MSSQL/MySQL & Sequelize</span>, Sci-Fi,{' '}
-      <span className="emphasize">Node.JS</span>, Cartoons,
+      <span className="emphasize">Node.JS</span>, Cartoons,{' '}
       <span className="emphasize">Express</span>, History,{' '}
       <span className="emphasize">Handlebars & EJS</span>, Retro Video Games,{' '}
       <span className="emphasize">MongoDB & Mongoose</span>...
